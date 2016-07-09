093dd64b0dc379cc8b64665d48c15394dab337f5
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-07-10 08:14:12 +0900

    clean codes

--- test.js
+++ test.js
@@ -13,6 +13,12 @@ function ok(s, c) {
   return true;
 }
 
+// EqualClause
+ok('name = "John"', { type:'eq', field:'name', value:'John' });
+ok('age = 30', { type:'eq', field:'age', value:30 });
+ok('X = "a \\"b\\" c"', { type:'eq', field:'X', value:'a "b" c' });
+ok('X="abc"', { type:'eq', field:'X', value:'abc' });
+
 // number (int+frac+exp)
 ok('X=1.23e45', {type:'eq', field:'X', value:1.23e+45});
 ok('X=1.23e+45', {type:'eq', field:'X', value:1.23e+45});
@@ -20,17 +26,12 @@ ok('X=1.23e-45', {type:'eq', field:'X', value:1.23e-45});
 ok('X=-1.23e45', {type:'eq', field:'X', value:-1.23e+45});
 ok('X=-1.23e+45', {type:'eq', field:'X', value:-1.23e+45});
 ok('X=-1.23e-45', {type:'eq', field:'X', value:-1.23e-45});
-ok('X=+1.23e45', {type:'eq', field:'X', value:1.23e+45});
-ok('X=+1.23e+45', {type:'eq', field:'X', value:1.23e+45});
-ok('X=+1.23e-45', {type:'eq', field:'X', value:1.23e-45});
 // number (int+exp)
 ok('X=1e23', {type:'eq', field:'X', value:1e+23});
 ok('X=1e+23', {type:'eq', field:'X', value:1e+23});
 ok('X=1e-23', {type:'eq', field:'X', value:1e-23});
 ok('X=-1e+23', {type:'eq', field:'X', value:-1e+23});
 ok('X=-1e-23', {type:'eq', field:'X', value:-1e-23});
-ok('X=+1e+23', {type:'eq', field:'X', value:1e+23});
-ok('X=+1e-23', {type:'eq', field:'X', value:1e-23});
 // number (int+frac)
 ok('X=1.23', {type:'eq', field:'X', value:1.23});
 ok('X=-1.23', {type:'eq', field:'X', value:-1.23});
@@ -38,11 +39,13 @@ ok('X=-1.23', {type:'eq', field:'X', value:-1.23});
 ok('X=12345', {type:'eq', field:'X', value:12345});
 ok('X=-12345', {type:'eq', field:'X', value:-12345});
 
-// EqualClause
-ok('name = "John"', { type: 'eq', field: 'name', value: 'John' });
-ok('age = 30', { type: 'eq', field: 'age', value: 30 });
-ok('X = "a \\"b\\" c"', { type: 'eq', field: 'X', value: 'a "b" c' });
-ok('X="abc"', { type: 'eq', field: 'X', value: 'abc' });
+// TODO: should be failed
+ok('X=+1.23e45', {type:'eq', field:'X', value:1.23e+45});
+ok('X=+1.23e+45', {type:'eq', field:'X', value:1.23e+45});
+ok('X=+1.23e-45', {type:'eq', field:'X', value:1.23e-45});
+ok('X=+1e+23', {type:'eq', field:'X', value:1e+23});
+ok('X=+1e-23', {type:'eq', field:'X', value:1e-23});
+ok('X=+12345', {type:'eq', field:'X', value:-12345});
 
 // PrefixClause
 ok('name PREFIX "foo"', { type:'prefix', field:'name', prefix:'foo' });
@@ -59,11 +62,11 @@ ok('X<=34', {type:'range', field:'X', upperLimit:34, upperIncluded:true});
 ok('X>12', {type:'range', field:'X', lowerLimit:12, lowerIncluded:false});
 ok('X>=34', {type:'range', field:'X', lowerLimit:34, lowerIncluded:true});
 // RangeClause (between)
-ok('12<=X<=34', {type:'range', field:'X', lowerLimit:12, upperLimit: 34,
+ok('12<=X<=34', {type:'range', field:'X', lowerLimit:12, upperLimit:34,
   lowerIncluded:true, upperIncluded:true});
-ok('12<=X<34', {type:'range', field:'X', lowerLimit:12, upperLimit: 34,
+ok('12<=X<34', {type:'range', field:'X', lowerLimit:12, upperLimit:34,
   lowerIncluded:true, upperIncluded:false});
-ok('12<X<=34', {type:'range', field:'X', lowerLimit:12, upperLimit: 34,
+ok('12<X<=34', {type:'range', field:'X', lowerLimit:12, upperLimit:34,
   lowerIncluded:false, upperIncluded:true});
-ok('12<X<34', {type:'range', field:'X', lowerLimit:12, upperLimit: 34,
+ok('12<X<34', {type:'range', field:'X', lowerLimit:12, upperLimit:34,
   lowerIncluded:false, upperIncluded:false});
