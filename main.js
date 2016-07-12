9ce63ffb4d1ee5579b7276e0e03ca435a773f3aa
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-07-12 11:39:04 +0900

    playable examples

--- main.js
+++ main.js
@@ -6,12 +6,30 @@
     clause: "",
     error: "",
     debug: true,
+    examples: [
+      'name = "abc"',
+      'age = 123',
+      'name PREFIX "abc"',
+      '20 <= age < 30',
+      'name PREFIX "John" & age = 30',
+      'X=10 AND Y=20 OR Z=30',
+      'X=10 & Y=20 | Z=30',
+      '(X=10 & Y=20) | Z=30',
+      'NOT X=10',
+      '!X=10',
+      'X!=10',
+      'loc IN 10 FROM (123 456)',
+      'loc IN (123 456) - (456 789)',
+    ],
   };
 
   var main = new Ractive({
     el: '#main-container',
     template: "#main-template",
     data: data,
+    setQuery: function(v) {
+      this.set('query', v);
+    },
     clearQuery: function() {
       this.set('query', '');
     },
